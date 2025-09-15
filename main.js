bed5f508e83a44a391adf73b73b24a24f07ed39a
0xSaad <isaadmalkyxi@gmail.com>
2025-09-15 14:36:25 +0300

    App

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 380,
+    height: 620,
+    resizable: true,
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+  win.loadFile(path.join(__dirname, 'renderer', 'index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
---
+++ preload.js
@@ -0,0 +1,2 @@
+// You can expose safe APIs to the renderer here if needed.
+// For this simple calculator we don't need Node in the renderer.
